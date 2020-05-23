9ba0840edddf9d53faccd3375aaf4c0f7380ef32
Owner <owner@Owners-MacBook-Pro.local>
2020-05-23 19:04:37 -0400

    old mini js projects

---
+++ README.md
@@ -0,0 +1,73 @@
+# JS Games
+
+The purpose of this project is to experiment with animation using Javascript. 
+Animations can be created with DOM elements, SVG images, or using the HTML
+Canvas. These projects are built purely with HTML, CSS and JavaScript (No
+Frameworks).
+
+All games in this project have live demos (See below). But none of them are
+mobile responsive.
+
+## Games
+1. [Basically Mario](#basically-mario)
+2. [Tic-Tac-Toe](#tic-tac-toe)
+3. [Wizards Hangman](#wizards-hangman)
+
+## Basically Mario
+This is a platform game similar to the classic arcade game Super Mario, based
+on the Platformer project from Chapter 16 of Eloquent JavaScript.
+
+![](readme-images/basicallyMario.gif)
+
+[LIVE DEMO](https://skapoo01.github.io/jsGames/basicallyMario)
+
+Features
+1. Object Oriented Design and state management in JavaScript
+2. DOM Manipulation w/ JavaScript
+3. 8 awesome levels to play through
+
+Future Directions
+1. Improve graphics
+2. Add a monster
+3. Add music
+4. High score board at the end
+
+## Tic-Tac-Toe
+Classic 2-player Tic-Tac-Toe game built with HTML Dom Manipulation.
+
+![](readme-images/ticTacToe.gif)
+
+[LIVE DEMO](https://skapoo01.github.io/jsGames/ticTacToe)
+
+Features
+1. Supports 2 players
+2. DOM Manipulation w/ JavaScript
+3. CSS Animations
+
+Future Directions
+1. Add single player mode
+
+## Wizards Hangman
+A twist on the classic Hangman game: put your Harry Pottr knowledge to the test.
+Only true fans will conquer this game!
+
+![LIVE DEMO](https://skapoo01.github.io/jsGames/wizardsHangman)
+
+LIVE DEMO
+
+Features
+1. SVG image manipulation
+
+Future Directions
+1. Improve graphics
+2. Add hints
+3. Add high scores
+
+## Games Wishlist
+More classic games to experiment with, especially using HTML Canvas
+1. Speed typing
+2. Air Hockey
+3. Asteroids
+4. 2048
+5. Flappy Bird
+6. PacMan
---
+++ wizardsHangman/script.js
@@ -0,0 +1,138 @@
+/*
+*   Purpose:    To implement a Harry Potter themed Hangman game
+*   Filename:   script.js
+*   Date:       Aug 23rd, 2018
+*   Author:     Siddharth Kapoor
+*/
+
+// State variables
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+const finalMessageRevealWord = document.getElementById('final-message-reveal-word');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['alohamora', 'quidditch', 'hogwarts', 'butterbeer', 'hedwig', 
+                'dobby', 'dumbledore', 'sectusempra', 'stupify', 'horcrux',
+                'buckbeak', 'bludger', 'deluminator', 'griffindor',
+                'slytherin', 'gillyweed', 'wormtail', 'deatheater'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+let playable = true;
+
+const correctLetters = [];
+const wrongLetters = [];
+
+// Display the correctly guessed portion of the word
+function displayWord() {
+	wordEl.innerHTML = `
+    ${selectedWord
+			.split('')
+			.map(
+				letter => `
+          <span class="letter">
+            ${correctLetters.includes(letter) ? letter : ''}
+          </span>
+        `
+			)
+			.join('')}
+  `;
+
+	const innerWord = wordEl.innerText.replace(/[ \n]/g, '');
+
+	if (innerWord === selectedWord) {
+		finalMessage.innerText = 'Congratulations! You won! 😃';
+		popup.style.display = 'flex';
+
+		playable = false;
+	}
+}
+
+// Record and update incorrectly guessed letters
+function updateWrongLettersEl() {
+	// Display wrong letters
+	wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+  `;
+
+	// Display parts
+	figureParts.forEach((part, index) => {
+		const errors = wrongLetters.length;
+
+		if (index < errors) {
+			part.style.display = 'block';
+		} else {
+			part.style.display = 'none';
+		}
+	});
+
+	// Check if the Hangman is complete
+	if (wrongLetters.length === figureParts.length) {
+		finalMessage.innerText = 'Unfortunately you lost. 😕';
+		finalMessageRevealWord.innerText = `...the word was: ${selectedWord}`;
+		popup.style.display = 'flex';
+
+		playable = false;
+	}
+}
+
+// Show notification
+function showNotification() {
+	notification.classList.add('show');
+
+	setTimeout(() => {
+		notification.classList.remove('show');
+	}, 2000);
+}
+
+// Catch keys pressed by players
+window.addEventListener('keydown', e => {
+	if (playable) {
+		if (e.keyCode >= 65 && e.keyCode <= 90) {
+			const letter = e.key.toLowerCase();
+
+			if (selectedWord.includes(letter)) {
+				if (!correctLetters.includes(letter)) {
+					correctLetters.push(letter);
+
+					displayWord();
+				} else {
+					showNotification();
+				}
+			} else {
+				if (!wrongLetters.includes(letter)) {
+					wrongLetters.push(letter);
+
+					updateWrongLettersEl();
+				} else {
+					showNotification();
+				}
+			}
+		}
+	}
+});
+
+// Handle restarting the game
+playAgainBtn.addEventListener('click', () => {
+	playable = true;
+
+	// Reset state variables
+	correctLetters.splice(0);
+	wrongLetters.splice(0);
+
+	selectedWord = words[Math.floor(Math.random() * words.length)];
+
+	displayWord();
+
+	updateWrongLettersEl();
+
+	popup.style.display = 'none';
+});
+
+displayWord();
